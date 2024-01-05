d816f531e9143df1733ea11127234a3e6af8c37f
eduar2 <car.eduar2@gmail.com>
2024-01-05 11:53:23 -0500

    First backend version

---
+++ src/main/java/com/pichincha/evaluacion/EjercicioJavaBPApplication.java
@@ -0,0 +1,13 @@
+package com.pichincha.prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioJavaBPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioJavaBPApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pichincha/evaluacion/EjercicioJavaBPApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pichincha.evaluacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioJavaBPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
